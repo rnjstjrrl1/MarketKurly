ee535565b80f83922b60e60e5ba17c2fe00016b4
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-06-28 16:14:51 +0900

    Initial Commit

---
+++ MarketKurly/MarketKurlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketKurlyApp.swift
+//  MarketKurly
+//
+//  Created by 권석기 on 6/28/25.
+//
+
+import SwiftUI
+
+@main
+struct MarketKurlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
